637441b0b70b1128dc802997508e16dff466587e
Rauf Aliev <rauf_aliev@epam.com>
2023-11-29 19:49:30 -0500

    Initial Commit

---
+++ openai-hotkey/openai_hotkeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  openai_hotkeyApp.swift
+//  openai-hotkey
+//
+//  Created by Rauf Aliev on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct openai_hotkeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
